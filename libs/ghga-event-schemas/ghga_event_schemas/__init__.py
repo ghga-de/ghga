0bbefad36b684a21c763f497c0d0cf5ab41aeda0
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-01-18 09:51:50 +0100

    Fix/update dependencies (#30)
    Updated from template
    Bumped dependency versions

--- libs/ghga-event-schemas/ghga_event_schemas/__init__.py
+++ libs/ghga-event-schemas/ghga_event_schemas/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 - 2022 Universität Tübingen, DKFZ and EMBL
+# Copyright 2021 - 2023 Universität Tübingen, DKFZ and EMBL
 # for the German Human Genome-Phenome Archive (GHGA)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,4 +15,4 @@
 
 """A package that collects schemas used for events exchanges between GHGA service."""
 
-__version__ = "0.7.4"
+__version__ = "0.8.0"
