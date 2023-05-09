df3741fab844eab5611a931bb1858a92ab24270c
Kersten Breuer <kersten-breuer@outlook.com>
2023-05-09 16:21:38 +0200

    Define Config and DAO (GSI 102) (#1)
    * migrate from old repo
    
    * add devcontainer adaptions
    
    * fix devcontainer
    
    * Update readme
    
    Remove template remnants
    
    * Rename ResourceDaoPort
    
    ---------
    
    Co-authored-by: TheByronHimes <TheByronHimes@gmail.com>

---
+++ services/mass/mass/ports/outbound/dao.py
@@ -0,0 +1,41 @@
+# Copyright 2021 - 2023 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""ResourceDao Port and DaoCollectionPort"""
+from abc import ABC, abstractmethod
+
+from hexkit.protocols.dao import DaoNaturalId
+from typing_extensions import TypeAlias
+
+from mass.core.models import Resource
+
+ResourceDao: TypeAlias = DaoNaturalId[Resource]
+
+
+class DaoCollectionPort(ABC):
+    """Port for a DAO collection object"""
+
+    @abstractmethod
+    def get_dao(self, *, class_name: str) -> ResourceDao:
+        """Retrieve a ResourceDaoPort for the specified resource class name
+
+        Args:
+            class_name (str): name of the resource class
+
+        Returns:
+            A DAO for the specified resource (ResourceDaoPort)
+        """
+        ...
