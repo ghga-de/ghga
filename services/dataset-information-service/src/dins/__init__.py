6494d4af6c6f792e702de019a122a062fe979654
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2024-10-01 10:10:13 +0200

    Update src/dins/__init__.py
    Co-authored-by: Byron Himes <byro93@live.com>

--- services/dataset-information-service/src/dins/__init__.py
+++ services/dataset-information-service/src/dins/__init__.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Short description of package"""
+"""Provides public metadata about files registered with the Internal File Registry"""
 
 from importlib.metadata import version
 
