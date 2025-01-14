8161a4831b11aa9da5901cada477840a4a6b94b5
Pascal Kraft <35356122+SilverLinings89@users.noreply.github.com>
2025-01-14 18:08:07 +0100

    Adding page titles to the router. (#32)
    * Adding page titles to the router.
    
    * Migrating implementation to a Title strategy.
    
    * Move the title template to a constant and perform the string replacement in-place. Separates logic from data.

--- frontend/data-portal/src/app/app.config.ts
+++ frontend/data-portal/src/app/app.config.ts
@@ -11,17 +11,18 @@ import {
 } from '@angular/core';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, TitleStrategy } from '@angular/router';
 import { provideHttpCache, withHttpCacheInterceptor } from '@ngneat/cashew';
 
 import { withFetch } from '@angular/common/http';
 import { csrfInterceptor } from '@app/auth/services/csrf.service';
-import { routes } from './app.routes';
+import { routes, TemplatePageTitleStrategy } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
     provideHttpClient(
       withFetch(),
       withInterceptors([withHttpCacheInterceptor(), csrfInterceptor]),
--- frontend/data-portal/src/app/app.routes.ts
+++ frontend/data-portal/src/app/app.routes.ts
@@ -4,8 +4,9 @@
  * @license Apache-2.0
  */
 
-import { inject } from '@angular/core';
-import { Routes } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 import { AuthService } from '@app/auth/services/auth.service';
 
 export const routes: Routes = [
@@ -15,6 +16,7 @@ export const routes: Routes = [
       import('./portal/features/home-page/home-page.component').then(
         (m) => m.HomePageComponent,
       ),
+    title: 'Home',
   },
   {
     path: 'browse',
@@ -22,6 +24,7 @@ export const routes: Routes = [
       import('./metadata/features/metadata-browser/metadata-browser.component').then(
         (m) => m.MetadataBrowserComponent,
       ),
+    title: 'Browse Datasets',
   },
   // routes used in the authentication flows
   {
@@ -37,6 +40,7 @@ export const routes: Routes = [
       import('./auth/features/register/register.component').then(
         (m) => m.RegisterComponent,
       ),
+    title: 'Registration',
   },
   {
     path: 'setup-totp',
@@ -44,6 +48,7 @@ export const routes: Routes = [
       import('./auth/features/setup-totp/setup-totp.component').then(
         (m) => m.SetupTotpComponent,
       ),
+    title: 'Set up TOTP',
   },
   {
     path: 'confirm-totp',
@@ -51,6 +56,7 @@ export const routes: Routes = [
       import('./auth/features/confirm-totp/confirm-totp.component').then(
         (m) => m.ConfirmTotpComponent,
       ),
+    title: 'Confirm TOTP',
   },
   {
     path: '**',
@@ -61,3 +67,29 @@ export const routes: Routes = [
     title: 'GHGA | Page not found',
   },
 ];
+
+/**
+ * This strategy provides a simpler way to set the page title. The routes (above) still set a title but it uses a template syntax to extend the title by the GHGA Data Portal string.
+ */
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  TITLE_TEMPLATE = '#title | GHGA Data Portal';
+
+  /**
+   * Constructor for the strategy.
+   * @param title Creates the member called title that stores the current page title.
+   */
+  constructor(private readonly title: Title) {
+    super();
+  }
+  /**
+   * This gets called whenever the router requests a new title for a site.
+   * @param routerState passes the current route to the function.
+   */
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(this.TITLE_TEMPLATE.replace('#title', title));
+    }
+  }
+}
