6d46fc7aae111ef55b223def7d5a983b4ac938b5
ckaipf <camill@posteo.de>
2025-03-25 11:57:51 +0100

    Enhance version difference calculation to include prerelease segments
    - Modified the get_version_diff function to account for the prerelease part of versioning.
    - Updated the loop to iterate over major, minor, patch, and prerelease.
    - Adjusted logic to bump the minor version when a difference is found in the prerelease segment.

--- deploy/scripts/bump_charts.py
+++ deploy/scripts/bump_charts.py
@@ -110,11 +110,14 @@ def get_charts(chart_files: list[Path]) -> list[dict[str, any]]:
 def get_version_diff(version_a: VersionInfo, version_b: VersionInfo) -> VersionInfo:
     """Calculates the absolute version difference between two versions."""
     diff = VersionInfo.parse("0.0.0")
-    for part in ["major", "minor", "patch"]:
+    for part in ["major", "minor", "patch", "prerelease"]:
         if getattr(version_a, part) != getattr(version_b, part):
-            diff = diff.replace(
-                **{part: abs(getattr(version_a, part) - getattr(version_b, part))}
-            )
+            if part == "prerelease":
+                diff = diff.bump_patch()
+            else:
+                diff = diff.replace(
+                    **{part: abs(getattr(version_a, part) - getattr(version_b, part))}
+                )
             print(f"Diff between {version_a} and {version_b}: {diff}")
             break
     return diff
