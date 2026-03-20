2c7b29fb7c5dbb33e36106393501a88d2b53e7a1
Camill Kaipf <camill.kaipf@uni-tuebingen.de>
2026-03-20 15:53:31 +0100

    Refactor tests for base

--- deploy/base/tests/conftest.py
+++ deploy/base/tests/conftest.py
@@ -1,6 +1,8 @@
+import json
 from pathlib import Path
 import subprocess
 from typing import Any
+import hashlib
 
 import pytest
 import yaml
@@ -8,6 +10,8 @@ import yaml
 BASE_DIR = Path(__file__).resolve().parents[2]
 CASES_DIR = Path(__file__).resolve().parent / "cases"
 DUMMY_CHART_DIR = BASE_DIR / "base" / "tests" / "dummy"
+RENDER_TO_FILES = True
+
 
 def _load_yaml(path: Path) -> dict[str, Any]:
     with path.open("r", encoding="utf-8") as file:
@@ -26,6 +30,7 @@ def _deep_merge(base: Any, override: Any) -> Any:
         return result
     return override
 
+
 @pytest.fixture(scope="session")
 def _dependency_update():
     o = subprocess.run(
@@ -40,38 +45,91 @@ def _dependency_update():
         )
 
 
-def _render_chart_case(composed_values: dict[str, Any],) -> str:
+@pytest.fixture()
+def release_name():
+    return "test"
+
+
+def _render_chart_case(composed_values: dict[str, Any], release_name: str) -> str:
     output = subprocess.run(
-        ["helm", "template", "--namespace", "test", "--values", "-", str(DUMMY_CHART_DIR)],
+        [
+            "helm",
+            "template",
+            release_name,
+            "--namespace",
+            "test",
+            "--values",
+            "-",
+            str(DUMMY_CHART_DIR),
+        ],
         capture_output=True,
         text=True,
-        input=yaml.dump(composed_values)
+        input=yaml.dump(composed_values),
     )
+    if RENDER_TO_FILES:
+        sha = hashlib.sha256(
+            json.dumps(composed_values, sort_keys=True).encode()
+        ).hexdigest()
+        subprocess.run(
+            [
+                "helm",
+                "template",
+                release_name,
+                "--output-dir",
+                f"./rendered/{sha[:8]}/",
+                "--namespace",
+                "test",
+                "--values",
+                "-",
+                str(DUMMY_CHART_DIR),
+            ],
+            capture_output=True,
+            text=True,
+            input=yaml.dump(composed_values),
+        )
+
     if output.returncode != 0:
         raise AssertionError(
             f"Helm command failed: {' '.join(output.args)}\nstdout:\n{output.stdout}\nstderr:\n{output.stderr}"
         )
     return output.stdout
 
+
 def _load_objects_from_yaml(yaml_str: str) -> dict[str, Any]:
     objects = yaml.safe_load_all(yaml_str)
     objects_dict = {}
     for obj in objects:
         if obj.get("kind") in objects_dict:
-            raise ValueError(f"Duplicate object kind '{obj.get('kind')}' found in rendered manifests. All objects must have unique 'kind' fields.")
+            raise ValueError(
+                f"Duplicate object kind '{obj.get('kind')}' found in rendered manifests. All objects must have unique 'kind' fields."
+            )
         else:
             objects_dict[obj.get("kind")] = obj
     return objects_dict
 
+
 def compose_values(*filenames: str) -> dict[str, Any]:
     composed: dict[str, Any] = {}
     for name in filenames:
-        composed = _deep_merge(composed, _load_yaml(CASES_DIR / name))
+        composed = _deep_merge(composed, _load_yaml(CASES_DIR / "values" / name))
     return composed
 
+
 @pytest.fixture
-def rendered_chart(_dependency_update):
+def rendered_chart(_dependency_update, release_name):
     def _factory(*filenames: str):
         composed_values = compose_values(*filenames)
-        return _load_objects_from_yaml(_render_chart_case(composed_values))
+        return _load_objects_from_yaml(
+            _render_chart_case(composed_values, release_name)
+        )
+
+    return _factory
+
+
+@pytest.fixture
+def expected():
+    def _factory(case: str, key: str):
+        expected = _load_yaml(CASES_DIR / "expected" / f"{case}.yaml")
+        return expected[key]
+
     return _factory
--- deploy/base/tests/test_composable_cases.py
+++ deploy/base/tests/test_composable_cases.py
@@ -3,21 +3,40 @@ def test_config(rendered_chart):
     assert manifests["ConfigMap"]["data"]["config"] == "foo: bar\n"
     print(manifests["ConfigMap"]["data"]["config"])
 
+
 def test_extra_volume(rendered_chart):
     manifests = rendered_chart("extra_volume.yaml")
-    assert "test" == manifests["Deployment"]["spec"]["template"]["spec"]["volumes"][1]["name"]
-    assert "test" == manifests["CronJob"]["spec"]["jobTemplate"]["spec"]["template"]["spec"]["volumes"][1]["name"]
+    assert (
+        "test"
+        == manifests["Deployment"]["spec"]["template"]["spec"]["volumes"][1]["name"]
+    )
+    assert (
+        "test"
+        == manifests["CronJob"]["spec"]["jobTemplate"]["spec"]["template"]["spec"][
+            "volumes"
+        ][1]["name"]
+    )
 
-def test_kafka_user(rendered_chart):
+
+def test_kafka_user(rendered_chart, expected):
     manifests = rendered_chart()
     assert "KafkaUser" not in manifests
     manifests = rendered_chart("kafka_user.yaml")
-    expected_acl = {
-    "operations": ["Write", "Describe", "Create"],
-    "resource": {
-        "name": "baz",
-        "patternType": "literal",
-        "type": "topic"
-    }
-    }
-    assert expected_acl == manifests["KafkaUser"]["spec"]["authorization"]["acls"][0]
+    assert (
+        expected("kafka_user", "acls")
+        == manifests["KafkaUser"]["spec"]["authorization"]["acls"]
+    )
+    assert (
+        expected("kafka_user", "secretAnnotations").items()
+        <= manifests["KafkaUser"]["spec"]["template"]["secret"]["metadata"]["annotations"].items()
+    )
+
+
+def test_vault_agent(rendered_chart, release_name, expected):
+    manifests = rendered_chart("vault_enabled.yaml")
+    assert (
+        expected("vault_enabled", "podAnnotations").items()
+        <= manifests["Deployment"]["spec"]["template"]["metadata"][
+            "annotations"
+        ].items()
+    )
