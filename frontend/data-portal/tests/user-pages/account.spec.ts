309524153a64e312b1368b1923fc1c0abd2cdbe7
maryam-sheikhi <maryam.sheikhi.github@gmail.com>
2026-04-21 17:34:32 +0200

    Add space to mat-card title (#300)

--- frontend/data-portal/tests/user-pages/account.spec.ts
+++ frontend/data-portal/tests/user-pages/account.spec.ts
@@ -37,25 +37,34 @@ test('show account page when logged in', async ({ loggedInPage }) => {
 
   const heading = main.getByRole('heading', { level: 1 });
   await expect(heading).toHaveText('User Account');
-  const subHeading = main.getByRole('heading', { level: 2 });
+  const subHeading = main.getByRole('heading', { level: 2 }).first();
   await expect(subHeading).toContainText('Dr. John Doe');
 
   // add more tests after adding arial labels
 
-  await expect(main).toContainText('Email');
+  await expect(main.getByRole('heading', { level: 2, name: 'Email' })).toBeVisible();
   await expect(main).toContainText(
     'We will communicate with you via this email address: doe@home.org',
   );
 
-  await expect(main).toContainText('Independent Verification Addresses (IVAs)');
+  await expect(
+    main.getByRole('heading', {
+      level: 2,
+      name: 'Independent Verification Addresses (IVAs)',
+    }),
+  ).toBeVisible();
   await expect(main).toContainText('SMS: +441234567890004');
 
-  await expect(main).toContainText('Dataset Access');
+  await expect(
+    main.getByRole('heading', { level: 2, name: 'Dataset Access' }),
+  ).toBeVisible();
   await expect(main).toContainText(
     /For the dataset GHGAD12345678901235 from .* to .*\./,
   );
 
-  await expect(main).toContainText('Pending Access Requests');
+  await expect(
+    main.getByRole('heading', { level: 2, name: 'Pending Access Requests' }),
+  ).toBeVisible();
   await expect(main).toContainText(
     /For the dataset GHGAD12345678901235 from .* to .*\./,
   );
