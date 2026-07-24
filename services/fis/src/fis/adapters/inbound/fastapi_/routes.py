1cb08183c92b4ffc6723f43b3ce075bd08661b28
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2026-07-24 07:10:11 +0000

    Meaningless change

--- services/fis/src/fis/adapters/inbound/fastapi_/routes.py
+++ services/fis/src/fis/adapters/inbound/fastapi_/routes.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""FastAPI routes for S3 upload metadata ingest"""
+"""FastAPI routes for S3 upload metadata ingest."""
 
 import logging
 from typing import Annotated
