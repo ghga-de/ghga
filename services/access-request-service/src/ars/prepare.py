fadfe588983e64d166ca6c304e7e9cb9a9004f48
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2026-08-07 09:07:47 +0200

    Migrate to httpx2 (#57)

--- services/access-request-service/src/ars/prepare.py
+++ services/access-request-service/src/ars/prepare.py
@@ -20,6 +20,7 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager, nullcontext
 from typing import NamedTuple
 
+import httpx2
 from fastapi import FastAPI
 from ghga_service_commons.auth.ghga import AuthContext, GHGAAuthContextProvider
 from hexkit.providers.akafka import KafkaEventPublisher, KafkaEventSubscriber
@@ -55,12 +56,20 @@ async def prepare_access_request_dao(
 async def prepare_core(
     *,
     config: Config,
+    access_grants_transport: httpx2.AsyncBaseTransport | None = None,
 ) -> AsyncGenerator[AccessRequestRepositoryPort]:
-    """Constructs and initializes all core components and their outbound dependencies."""
+    """Constructs and initializes all core components and their outbound dependencies.
+
+    By default, the access grants adapter talks to the download access API over the
+    network, but you can intercept its requests using the access_grants_transport
+    parameter.
+    """
     async with (
         MongoDbDaoFactory.construct(config=config) as dao_factory,
         prepare_access_request_dao(config=config) as access_request_dao,
-        AccessGrantsAdapter.construct(config=config) as access_grants,
+        AccessGrantsAdapter.construct(
+            config=config, transport=access_grants_transport
+        ) as access_grants,
     ):
         dataset_dao = await get_dataset_dao(dao_factory=dao_factory)
         yield AccessRequestRepository(
--- services/access-request-service/tests/test_outbox_pub.py
+++ services/access-request-service/tests/test_outbox_pub.py
@@ -90,7 +90,7 @@ async def test_upsert(config, kafka: KafkaFixture, mongodb: MongoDbFixture):
         assert event.type_ == "upserted"
         assert event.key == event.payload["id"] == str(access_request.id)
         assert event.payload["status"] == "pending"
-        assert event.payload["ticket_id"] == None
+        assert event.payload["ticket_id"] is None
 
         # Perform an update to the request
         access_request_update = access_request.model_copy(
