5952fc8263b5cce0a7b0f9e98c77c565fac72955
TheByronHimes <TheByronHimes@gmail.com>
2026-09-11 15:37:25 +0000

    Fix doc string for process_file_deletion_requested

--- services/ucs/src/ucs/core/controller.py
+++ services/ucs/src/ucs/core/controller.py
@@ -1559,7 +1559,8 @@ class UploadController(UploadControllerPort):
         """Process a deletion request for the given FileUpload ID.
 
         This will remove the object from the inbox, if it exists.
-        Database objects are untouched.
+        FileUpload objects in the database are updated to the `cancelled` state
+        but not deleted.
 
         If no FileUpload with the given ID exists, merely logs a warning and returns.
         """
